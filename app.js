7de0fa0f4bbe90b56ede3c5dfb837fa6a552833d
nathanallen <thenathanator@gmail.com>
2015-07-02 16:20:49 -0700

    stubbed UserService

--- app.js
+++ app.js
@@ -14,12 +14,26 @@ app.config(function($routeProvider){
 
 })
 
-app.controller('PokemonShowCtrl', function($scope, $routeParams, $http){
+app.factory('UserService', function(){
+
+  var UserService = {};
+
+  UserService.current_user = function(){
+    return {
+      api_key: "WDI18RULES"
+    };
+  }
+
+  return UserService;
+
+})
+
+app.controller('PokemonShowCtrl',function($scope, $routeParams, $http, UserService){
   var id = $routeParams.id;
   var endpoint = "https://pokemon-api.herokuapp.com/pokemons/"
 
   $http
-      .get(endpoint + id, {params: { api_key: "WDI18RULES" }})
+      .get(endpoint + id, {params: { api_key: UserService.current_user().api_key }})
       .success(function(response){
           $scope.hello = response.name;
       })
