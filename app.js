5ed6ee943d017328edb33545d7ef47c407840a59
nathanallen <thenathanator@gmail.com>
2015-07-02 16:04:37 -0700

    pokemonShowCtrl

--- app.js
+++ app.js
@@ -9,12 +9,26 @@ app.config(function($routeProvider){
     })
     .when('/pokemon/:id', {
       template: '{{hello}}',
-      controller: 'MainCtrl'
+      controller: 'PokemonShowCtrl'
     })
+
+})
+
+app.controller('PokemonShowCtrl', function($scope, $routeParams, $http){
+  var id = $routeParams.id;
+  var endpoint = "https://pokemon-api.herokuapp.com/pokemons/127?api_key=WDI18RULES"
+
+  $http
+      .get(endpoint)
+      .success(function(response){
+          $scope.hello = response.name;
+      })
+      .error(function(rejection) {
+          $scope.hello = "ERROR";
+      });
 
 })
 
 app.controller('MainCtrl', function($scope, $routeParams){
   $scope.hello = $routeParams.id;
 })
-
